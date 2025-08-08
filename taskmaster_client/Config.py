16210e310ef7fe2e6b0f790c798d7ca24721bbb1
dtome-pe <theonewhoknew@protonmail.com>
2025-08-08 11:50:37 +0200

    try catch added for file not found error

--- taskmaster_client/Config.py
+++ taskmaster_client/Config.py
@@ -7,21 +7,30 @@ class Config:
 
     @staticmethod
     def parse(path : str):
-        with open(path) as stream:
-            try:
-                data : dict = yaml.safe_load(stream)
-            except yaml.YAMLError as exc:
-                print(exc)
+        try:
+            with open(path) as stream:
+                try:
+                    data : dict = yaml.safe_load(stream)
+                except yaml.YAMLError as exc:
+                    print(exc)
+        except FileNotFoundError as exc:
+            print(f"Configuration file not found: {path}")
+            return
+        except Exception as e:
+            print(f"Unexpected error while opening config file: {e}")
+            return
+                
 
-            client = data.get('client')
-            if not client:
-                print("No client section found in configuration file")
-                return
-            
-            server_url = client.get('serverurl')
-            if not server_url:
-                print("No server url was specified")
-                return
-            
-            Config.url = server_url
-            Config.ok = True
+        client = data.get('client')
+        if not client:
+            print("No client section found in configuration file")
+            return
+        
+        server_url = client.get('serverurl')
+        if not server_url:
+            print("No server url was specified")
+            return
+        
+        Config.url = server_url
+        Config.ok = True
+     
